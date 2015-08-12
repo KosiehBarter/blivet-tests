c3e173f39bcee5fc18eff1ff67f0edc0ca34511a
Karel Valek <kvalek@redhat.com>
2015-08-12 12:02:40 +0200

    Disk and partitions classes definition
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- classes.py
+++ classes.py
@@ -1,10 +1,9 @@
-### Single disk - single partition
+### Classes definition
 ### Part of: Blivet test collection
 ### Author: kvalek@redhat.com
-### All programs I create are under GPL license.
+### This program is under GPL licence.
 import test_utils
 import blivet_test_utils
-import checker_test_utils
 
 class SystemDisk(object): # Uppercase znaky nevadi, bez podtrzitek
     """SystemDisk class contains all data regarding to a disk.
@@ -15,23 +14,56 @@ class SystemDisk(object): # Uppercase znaky nevadi, bez podtrzitek
         
         :param str disk: disk's name
         """
-        self.name = disk # Disk name
-        self.sector_size = int(test_utils.cat_data("/sys/block/{}/queue/hw_sector_size".format(disk))) # disk sector size
-        self.num_of_sectors = int(test_utils.cat_data("/sys/block/{}/size".format(disk))) # Disk num of sectors
-        self.system_path = test_utils.ls_path("/dev/{}".format(disk)) # Disk system path
-        self.removable = int(test_utils.cat_data("/sys/block/{}/removable".format(disk))) # Removable disk?
-        self.vendor = test_utils.cat_data("/sys/block/{}/device/vendor".format(disk)) # Disk vendor
-        self.model = test_utils.cat_data("/sys/block/{}/device/vendor".format(disk)) # Disk model
-        self.serial_num = None     # Disk S/N
-        self.space = self.sector_size * self.num_of_sectors # Disk size
+        self.name = disk
+        self.sector_size = int(test_utils.cat_data("/sys/block/{}/queue/hw_sector_size".format(disk)))
+        self.system_path = test_utils.ls_path("/dev/{}".format(disk))
+        self.num_of_sectors = int(test_utils.cat_data("/sys/block/{}/size".format(disk)))
+        self.removable = test_utils.cat_data_boolean("/sys/block/{}/removable".format(disk))
+        self.vendor = test_utils.cat_data("/sys/block/{}/device/vendor".format(disk))
+        self.space = self.sector_size * self.num_of_sectors
         self.children = []
+        
+class SystemDiskFormatted(SystemDisk):
+    def __init__(self):
+        #self.model = test_utils.cat_data("/sys/block/{}/device/vendor".format(disk))
+        self.serial_num = None
 
 class SystemPartition(object):
+    """SystemPartition contains data regarding to a partition.
+    This object defines what is a partition and what kind of data are
+    related to a partition."""
     def __init__(self, disk, part_num):
+        """
+        
+        :param str disk: disk's name
+        :param int part_num: partition number
+        """
         self.name = "{}{}".format(disk, part_num)
-        self.num_of_sectors = None
+        #self.num_of_sectors = 
         self.system_path = None
         self.space = None
         self.parent = disk
 
+class BlivetDisk(object):
+    """BlivetDisk is object containing data related to a disk,
+    how Blivet recognizes it."""
+    def __init__(self, disk):
+        """
+        
+        :param str disk: disk's name
+        """
+        self.b_object = blivet_test_utils.init_blivet()
+        self.b_object.reset()
+        self.b_disk = blivet_test_utils.get_dev_by_name(self.b_object, disk)
+        self.b_name = self.b_disk.name
+        self.b_system_path = self.b_disk.path
+        self.b_removable = self.b_disk.removable
+        self.b_vendor = self.b_disk.vendor
+        self.b_model = self.b_disk.model
+        self.b_space = int(self.b_disk.size)
+        self.b_children = []
 
+class BlivetDiskFormatted(BlivetDisk):
+    def __init__(self):
+        #self.b_num_of_sectors = self.b_disk. # TODO
+        self.b_serial_num = self.b_disk.serial
