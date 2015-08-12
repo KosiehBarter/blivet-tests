d30dda778e98d738b6a5e4c3058d93e54ba545b5
Karel Valek <kvalek@redhat.com>
2015-08-12 12:05:01 +0200

    Blivet utilities library for Blivet tests.
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- blivet_test_utils.py
+++ blivet_test_utils.py
@@ -1,7 +1,7 @@
 ### Test utils - Blivet
 ### Part of: Blivet test collection
 ### Author: kvalek@redhat.com
-### All programs I create are under GPL license.
+### This program is under GPL licence.
 import test_utils
 import sys
 import blivet
@@ -13,8 +13,11 @@ def init_blivet():
 ## Get device by name
 def get_dev_by_name(blivet_object, disk):
     return blivet_object.devicetree.getDeviceByName(disk)
-    
 
 ## Scan disk kids
 def get_dev_kids(blivet_object, scanned_disk):
     return blivet_object.devicetree.getChildren(scanned_disk)
+
+def get_dev_alloc_size(blivet_object, disk):
+    return blivet_object.formats.disklabel.DiskLabel
+    
