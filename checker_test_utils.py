d4207f442ac10ec8ac787538c48aa8176e37c98e
Karel Valek <kvalek@redhat.com>
2015-08-12 12:05:42 +0200

    Library containing checking tools for Blivet tests
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- checker_test_utils.py
+++ checker_test_utils.py
@@ -1,16 +1,20 @@
 ### Test utils - Checking tool
 ### Part of: Blivet test collection
 ### Author: kvalek@redhat.com
-### All programs I create are under GPL license.
+### This program is under GPL licence.
 
-## Print information
-def print_information(issue_array):
-    for inc in issue_array:
-        print(inc)
-
-## Check size
-def check_dev_size(ia, value_a, value_b, scanned_dev):
-    if (value_a == value_b):
-        ia.append("PASS:\tSIZE:\t{} == {} on dev {}".format(value_a, value_b, scanned_dev))
-    else:
-        ia.append("FAIL:\tSIZE NOT EQUAL: size {} != {} size on dev {}".format(value_a, value_b, scanned_dev))
+def test_properties(sys_scan, blv_scan):
+    ia = []
+    ss = [sys_scan.name, sys_scan.system_path, sys_scan.removable, sys_scan.vendor, sys_scan.space]
+    bs = [blv_scan.b_name, blv_scan.b_system_path, blv_scan.b_removable, blv_scan.b_vendor, blv_scan.b_space]
+    
+    for inc in range(len(ss)):
+        if (ss[inc] == bs[inc]):
+            ia.append("PASS: {} == {}".format(ss[inc], bs[inc]))
+        else:
+            ia.append("FAIL: {} != {}".format(ss[inc], bs[inc]))
+    return ia
+    
+def print_properties(ia):
+    for inc in range(len(ia)):
+        print(ia[inc])
