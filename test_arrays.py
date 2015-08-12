a8e4962598e5371280e28c638a152a5ecfa71cc3
Karel Valek <kvalek@redhat.com>
2015-08-12 16:59:24 +0200

    Added test and defs for classes
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_arrays.py
+++ test_arrays.py
@@ -10,3 +10,11 @@ test_blivet = classes.BlivetDisk('vdb')
 
 ia = checker_test_utils.test_properties(test_system, test_blivet)
 checker_test_utils.print_properties(ia)
+
+test_mbr_dos = classes.SystemDiskFormatted(disk) ## TODO: FIX
+test_mbr_blv = classes.BlivetDiskFormatted()
+
+if test_mbr_dos.alloc_type == test_mbr_blv.b_alloc_type:
+    print("PASS")
+else:
+    print("FAIL")
