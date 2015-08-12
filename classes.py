92580e365709cca74258381eaa5b52124289521b
Karel Valek <kvalek@redhat.com>
2015-08-12 16:50:37 +0200

    Added classes for Blivet
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- classes.py
+++ classes.py
@@ -5,14 +5,13 @@
 import test_utils
 import blivet_test_utils
 
-class SystemDisk(object): # Uppercase znaky nevadi, bez podtrzitek
-    """SystemDisk class contains all data regarding to a disk.
-    At start, a "disk" parameter is received. Based on this parameter,
-    all required data are gathered."""
+class SystemDisk(object):
+    """ Contains all data regarding to a disk.
+        At start, a "disk" parameter is received. Based on this parameter,
+        all required data are gathered."""
     def __init__(self, disk):
         """
-        
-        :param str disk: disk's name
+            :param str disk: disk's name
         """
         self.name = disk
         self.sector_size = int(test_utils.cat_data("/sys/block/{}/queue/hw_sector_size".format(disk)))
@@ -22,35 +21,40 @@ class SystemDisk(object): # Uppercase znaky nevadi, bez podtrzitek
         self.vendor = test_utils.cat_data("/sys/block/{}/device/vendor".format(disk))
         self.space = self.sector_size * self.num_of_sectors
         self.children = []
-        
+
+
 class SystemDiskFormatted(SystemDisk):
-    def __init__(self):
-        #self.model = test_utils.cat_data("/sys/block/{}/device/vendor".format(disk))
-        self.serial_num = None
+    """ Contains formatted disk object as well as data related to this
+        object"""
+    def __init__(self, disk):
+        test_utils.create_new_alloc_table(disk)
+        self.alloc_type = test_utils.get_alloc_type(disk)
+
 
 class SystemPartition(object):
-    """SystemPartition contains data regarding to a partition.
-    This object defines what is a partition and what kind of data are
-    related to a partition."""
+    """ Contains data regarding to a partition.
+        This object defines what is a partition and what kind of data are
+        related to a partition."""
     def __init__(self, disk, part_num):
         """
-        
-        :param str disk: disk's name
-        :param int part_num: partition number
+            :param str disk: disk's name
+            :param int part_num: partition number
         """
         self.name = "{}{}".format(disk, part_num)
-        #self.num_of_sectors = 
-        self.system_path = None
-        self.space = None
+        self.system_path = test_utils.ls_path("/dev/{}{}".format(disk, part_num))
+        self.part_num_of_sectors = int(test_utils.cat_data("/sys/block/{}/{}{}/size".format(disk, disk, part_num)))
+        self.part_type = None
+        self.part_start = None
+        self.part_end = None
         self.parent = disk
 
+
 class BlivetDisk(object):
-    """BlivetDisk is object containing data related to a disk,
-    how Blivet recognizes it."""
+    """ Contains data related to a disk,
+        how Blivet recognizes it."""
     def __init__(self, disk):
         """
-        
-        :param str disk: disk's name
+            :param str disk: disk's name
         """
         self.b_object = blivet_test_utils.init_blivet()
         self.b_object.reset()
@@ -63,7 +67,25 @@ class BlivetDisk(object):
         self.b_space = int(self.b_disk.size)
         self.b_children = []
 
+
 class BlivetDiskFormatted(BlivetDisk):
+    """ Contains gathered information by Blivet related to formatted
+        disk."""
     def __init__(self):
-        #self.b_num_of_sectors = self.b_disk. # TODO
-        self.b_serial_num = self.b_disk.serial
+        self.b_alloc_type = self.b_disk.format.partedDisk.type
+
+class BlivetPartition(object):
+    """ Contains data related to partitions, as Blivet recognizes it."""
+    def __init__(self, disk):
+        """
+            :param str disk: disk's name
+        """
+        ## TODO: Complete
+        self.b_name = None
+        self.b_system_path = None
+        self.b_num_of_sectors = None
+        self.b_part_type = None
+        self.b_part_start = None
+        self.b_part_end = None
+        self.parent = None
+        
