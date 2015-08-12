ce6337da20e644bab5320651bd25997627593ad6
Karel Valek <kvalek@redhat.com>
2015-08-12 16:49:29 +0200

    Library containing checking tools for Blivet tests
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- checker_test_utils.py
+++ checker_test_utils.py
@@ -4,15 +4,16 @@
 ### This program is under GPL licence.
 
 def test_properties(sys_scan, blv_scan):
+    lt = ["NAME", "PATH", "REMOVABLE", "VENDOR", "SIZE"]
     ia = []
     ss = [sys_scan.name, sys_scan.system_path, sys_scan.removable, sys_scan.vendor, sys_scan.space]
     bs = [blv_scan.b_name, blv_scan.b_system_path, blv_scan.b_removable, blv_scan.b_vendor, blv_scan.b_space]
     
     for inc in range(len(ss)):
         if (ss[inc] == bs[inc]):
-            ia.append("PASS: {} == {}".format(ss[inc], bs[inc]))
+            ia.append("PASS: {}\t: {} == {}".format(lt[inc], ss[inc], bs[inc]))
         else:
-            ia.append("FAIL: {} != {}".format(ss[inc], bs[inc]))
+            ia.append("FAIL: {}\ŧ {} != {}".format(lt[inc], ss[inc], bs[inc]))
     return ia
     
 def print_properties(ia):
