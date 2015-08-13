dbbe19f1ac0cc270a8a839b41641e2a1a6402f9b
Karel Valek <kvalek@redhat.com>
2015-08-13 15:13:17 +0200

    Added crude test for partition table
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_arrays.py
+++ test_arrays.py
@@ -8,13 +8,10 @@ import checker_test_utils
 test_system = classes.SystemDisk('vdb')
 test_blivet = classes.BlivetDisk('vdb')
 
-ia = checker_test_utils.test_properties(test_system, test_blivet)
+ia = checker_test_utils.test_properties_disk(test_system, test_blivet)
 checker_test_utils.print_properties(ia)
 
-test_mbr_dos = classes.SystemDiskFormatted(disk) ## TODO: FIX
-test_mbr_blv = classes.BlivetDiskFormatted()
-
-if test_mbr_dos.alloc_type == test_mbr_blv.b_alloc_type:
-    print("PASS")
-else:
-    print("FAIL")
+test_system_formatted = classes.SystemDiskFormatted('vdb')
+test_blivet_formatted = classes.BlivetDiskFormatted('vdb')
+print(test_system_formatted.alloc_type)
+print(test_blivet_formatted.b_alloc_type)
