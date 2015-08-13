758cf1d11c8c96166aa46ef6f5c9971e528cedcb
Karel Valek <kvalek@redhat.com>
2015-08-13 16:52:23 +0200

    Implementing basic system and blivet test.
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- checker_test_utils.py
+++ checker_test_utils.py
@@ -8,14 +8,27 @@ def test_properties_disk(sys_scan, blv_scan):
     ia = []
     ss = [sys_scan.name, sys_scan.system_path, sys_scan.removable, sys_scan.vendor, sys_scan.space]
     bs = [blv_scan.b_name, blv_scan.b_system_path, blv_scan.b_removable, blv_scan.b_vendor, blv_scan.b_space]
-    
+
     for inc in range(len(ss)):
         if (ss[inc] == bs[inc]):
             ia.append("PASS: {}\t: {} == {}".format(lt[inc], ss[inc], bs[inc]))
         else:
-            ia.append("FAIL: {}\ŧ {} != {}".format(lt[inc], ss[inc], bs[inc]))
+            ia.append("FAIL: {}\t {} != {}".format(lt[inc], ss[inc], bs[inc]))
     return ia
-    
+
 def print_properties(ia):
     for inc in range(len(ia)):
         print(ia[inc])
+
+def test_properties_partition(sys_scan, blv_scan):
+    lt = ["NAME", "PATH", "num_of_sectors", "PART_TYPE", "PART_START", "PART_END"]
+    ia = []
+    ss = [sys_scan.name, sys_scan.system_path, sys_scan.num_of_sectors, sys_scan.part_type, sys_scan.part_start, sys_scan.part_end]
+    bs = [blv_scan.name, blv_scan.system_path, blv_scan.num_of_sectors, blv_scan.part_type, blv_scan.part_start, blv_scan.part_end]
+
+    for inc in range(len(ss)):
+        if (ss[inc] == bs[inc]):
+            ia.append("PASS: {}\t: {} == {}".format(lt[inc], ss[inc], bs[inc]))
+        else:
+            ia.append("FAIL: {}\t: {} != {}".format(lt[inc], ss[inc], bs[inc]))
+    return ia
