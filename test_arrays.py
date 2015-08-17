20311103194e4d927580b20faaff610f808cb4f2
Karel Valek <kvalek@redhat.com>
2015-08-17 12:35:59 +0200

    Changed vda to vdb
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_arrays.py
+++ test_arrays.py
@@ -14,8 +14,10 @@ checker_test_utils.print_properties(ia)
 test_system_formatted = classes.SystemDiskFormatted('vdb')
 test_blivet_formatted = classes.BlivetDiskFormatted('vdb')
 
-test_system_partition = classes.SystemPartition('vda', 1)
-test_blivet_partition = classes.BlivetPartition('vda', 1)
+partition_creator = classes.SystemPartitionCreate('vdb', 1, -1, "MiB", "primary")
+
+test_system_partition = classes.SystemPartition('vdb', 1)
+test_blivet_partition = classes.BlivetPartition('vdb', 1)
 
 ia = checker_test_utils.test_properties_partition(test_system_partition, test_blivet_partition)
 checker_test_utils.print_properties(ia)
