db8bbcedeea4fdac1c25b5b57c9ed9e7cd6ddebe
Karel Valek <kvalek@redhat.com>
2015-08-18 10:55:36 +0200

    Added single part test entries
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_arrays.py
+++ test_arrays.py
@@ -5,19 +5,29 @@
 import classes
 import checker_test_utils
 
+print("Basic disk check")
 test_system = classes.SystemDisk('vdb')
 test_blivet = classes.BlivetDisk('vdb')
 
 ia = checker_test_utils.test_properties_disk(test_system, test_blivet)
 checker_test_utils.print_properties(ia)
 
+print("\nFormatted disk check")
 test_system_formatted = classes.SystemDiskFormatted('vdb')
 test_blivet_formatted = classes.BlivetDiskFormatted('vdb')
+ia = checker_test_utils.check_formatting(test_system_formatted, test_blivet_formatted)
 
+
+print("\nPartitioned disk check - Single partition")
 partition_creator = classes.SystemPartitionCreate('vdb', 1, -1, "MiB", "primary")
-
-test_system_partition = classes.SystemPartition('vdb', 1)
+test_system_partition = classes.SystemPartition('vdb', 1, 1, -1, "MiB", "primary")
 test_blivet_partition = classes.BlivetPartition('vdb', 1)
+ia = checker_test_utils.test_properties_partition(test_system_partition, test_blivet_partition)
+checker_test_utils.print_properties(ia)
 
+print("\nPartitioned disk check - Single Formatted")
+partition_formatter = classes.SystemPartitionFormatted('vdb', 1, 1, -1, "MiB", "primary", "ext4")
+test_system_partition = classes.SystemPartition('vdb', 1, 1, -1, "MiB", "primary")
+test_blivet_partition = classes.BlivetPartition('vdb', 1)
 ia = checker_test_utils.test_properties_partition(test_system_partition, test_blivet_partition)
 checker_test_utils.print_properties(ia)
