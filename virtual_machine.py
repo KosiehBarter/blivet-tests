c6d7374deb24f4f5d72e79820e3197e434064e81
Karel Valek <kvalek@redhat.com>
2015-08-19 16:17:51 +0200

    Added script for automated install - NOT WORKING
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- virtual_machine.py
+++ virtual_machine.py
@@ -3,8 +3,32 @@
 ### Author: kvalek@redhat.com
 ### This program is under GPL licence.
 
+install_name = "f23"
+install_disk = "{}.iso".format(install_name)
+mach_name = install_name
+full_disk_path_1 = "/home/Libvirt_disks/{}1.img".format(mach_name)
+full_disk_path_2 = "/home/Libvirt_disks/{}2.img".format(mach_name)
+full_disk_path_3 = "/home/Libvirt_disks/{}3.img".format(mach_name)
+full_loc_path = "/home/Libvirt_disks/{}".format(install_disk)
+ks_file_path = "http://cobra02/ks/kv/autoInstaller.ks"
+ram_size = 2048
+
 import subprocess
 
-subprocess.call(["virt-install", "--name", "{}".format(mach_name), "--disk",\
- "{}".format(full_disk_path), "--location", "{}".format(full_loc_path),\
- "--ram", "{}".format(ram_size), "-x", "{}".format(ks_file_path)])
+# Destroy the machine if needed
+subprocess.call(["virsh", "undefine", "{}".format(mach_name)])
+subprocess.call(["virsh", "destroy", "{}".format(mach_name)])
+
+#subprocess.call(["virt-install", "--name", "{}".format(mach_name),\
+# "--disk", "{},size=8".format(full_disk_path_1),\
+# "--disk", "{},size=2".format(full_disk_path_2),\
+# "--disk", "{},size=2".format(full_disk_path_3),\
+# "--location", "{}".format(full_loc_path),\
+# "--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path)])
+
+print("virt-install", "--name", "{}".format(mach_name),\
+ "--disk", "{},size=8".format(full_disk_path_1),\
+ "--disk", "{},size=2".format(full_disk_path_2),\
+ "--disk", "{},size=2".format(full_disk_path_3),\
+ "--location", "{}".format(full_loc_path),\
+ "--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path))
