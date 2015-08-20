3a687d0ec14e668f5fe19d4eade424cd0d2cd82b
Karel Valek <kvalek@redhat.com>
2015-08-20 13:39:51 +0200

    Fixed: Reboot and basic networking.
    Changes in net configs in virsh-net
    
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- virtual_machine.py
+++ virtual_machine.py
@@ -14,21 +14,27 @@ ks_file_path = "http://cobra02/ks/kv/autoInstaller.ks"
 ram_size = 2048
 
 import subprocess
+import glob
+import time
+
+#
+scp_source = "/home/kvalek/GitHub/blivet-tests/"
+
 
 # Destroy the machine if needed
-subprocess.call(["virsh", "undefine", "{}".format(mach_name)])
 subprocess.call(["virsh", "destroy", "{}".format(mach_name)])
+subprocess.call(["virsh", "undefine", "{}".format(mach_name)])
 
-#subprocess.call(["virt-install", "--name", "{}".format(mach_name),\
-# "--disk", "{},size=8".format(full_disk_path_1),\
-# "--disk", "{},size=2".format(full_disk_path_2),\
-# "--disk", "{},size=2".format(full_disk_path_3),\
-# "--location", "{}".format(full_loc_path),\
-# "--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path)])
+subprocess.call(["virt-install", "--name", "{}".format(mach_name),\
+"--disk", "{},size=8".format(full_disk_path_1),\
+"--disk", "{},size=2".format(full_disk_path_2),\
+"--disk", "{},size=2".format(full_disk_path_3),\
+"--location", "{}".format(full_loc_path),\
+"--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path), "--noreboot"])
 
-print("virt-install", "--name", "{}".format(mach_name),\
- "--disk", "{},size=8".format(full_disk_path_1),\
- "--disk", "{},size=2".format(full_disk_path_2),\
- "--disk", "{},size=2".format(full_disk_path_3),\
- "--location", "{}".format(full_loc_path),\
- "--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path))
+subprocess.call(["virsh", "start", install_name])
+time.sleep(60)
+file_list = glob.glob("{}*".format(scp_source))
+for inc in file_list:
+    subprocess.call(["scp", inc, "{}:~".format(install_name)])
+subprocess.call(["virsh", "snapshot-create-as", install_name, "--disk-only", "--atomic"])
