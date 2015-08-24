e0b975c689f83cda1ff44eb2a1c06f53af1015b6
Karel Valek <kvalek@redhat.com>
2015-08-24 11:47:33 +0200

    Added: scp results back, fixed: IP address obtain
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- virtual_machine.py
+++ virtual_machine.py
@@ -10,38 +10,46 @@ full_disk_path_1 = "/home/Libvirt_disks/{}1.img".format(mach_name)
 full_disk_path_2 = "/home/Libvirt_disks/{}2.img".format(mach_name)
 full_disk_path_3 = "/home/Libvirt_disks/{}3.img".format(mach_name)
 full_loc_path = "/home/Libvirt_disks/{}".format(install_disk)
+
 ks_file_path = 'http://10.34.39.2/ks/kv/autoInstaller.ks'
+key_location = "/home/kvalek/.ssh/atlas"
 ram_size = 2048
 
 import subprocess
 import glob
 import time
+import re
 
 #
 scp_source = "/home/kvalek/GitHub/blivet-tests/"
 
 
 # Destroy the machine if needed
-subprocess.call(["virsh", "destroy", "{}".format(mach_name)])
-subprocess.call(["virsh", "undefine", "{}".format(mach_name)])
+#subprocess.call(["virsh", "destroy", "{}".format(mach_name)])
+#subprocess.call(["virsh", "undefine", "{}".format(mach_name)])
 
-subprocess.call(["virt-install", "--name", "{}".format(mach_name),\
-"--disk", "{},size=8".format(full_disk_path_1),\
-"--disk", "{},size=2".format(full_disk_path_2),\
-"--disk", "{},size=2".format(full_disk_path_3),\
-"--location", "{}".format(full_loc_path),\
-"--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path), "--noreboot"])
+#subprocess.call(["virt-install", "--name", "{}".format(mach_name),\
+#"--disk", "{},size=8".format(full_disk_path_1),\
+#"--disk", "{},size=2".format(full_disk_path_2),\
+#"--disk", "{},size=2".format(full_disk_path_3),\
+#"--location", "{}".format(full_loc_path),\
+#"--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path), "--noreboot"])
 
 
-subprocess.call(["virsh", "start", install_name])
-time.sleep(20)
-ip_address = subprocess.getoutput("virt-log -d f23 | grep bound | tail -n 1 | sed -r 's/.*to\ \(.*\)\ --.*/\1/'")
+#subprocess.call(["virsh", "start", install_name])
+#time.sleep(20)
+ip_address = subprocess.getoutput("virt-log -d f23 | grep bound | tail -n 1")
+vystup = re.search(r'.*to\ ([0-9]*.[0-9]*.[0-9*]*.[0-9]*).*', ip_address)
+if vystup:
+    print(vystup.group(1))
 print(ip_address)
 file_list = glob.glob("{}*".format(scp_source))
 for inc in file_list:
-    subprocess.call(["scp", "-i", "/home/kvalek/.ssh/atlas", inc, "{}:~".format(ip_address)])
+    subprocess.call(["scp", "-i", "{}".format(key_location), inc, "{}:~".format(vystup.group(1))])
 subprocess.call(["virsh", "snapshot-create-as", install_name, "--disk-only", "--atomic"])
-subprocess.call(["ssh", "-i", "/home/kvalek/.ssh/atlas", "root@{}".format(ip_address), "'python3'","'test_arrays.py'"])
+subprocess.call(["ssh", "-i", "{}".format(key_location), "root@{}:~".format(vystup.group(1)), "'python3'","'test_arrays.py'"])
+subprocess.call(["scp", "-i", "{}".format(key_location), "root@{}:/root/TEST_RESULT".format(vystup.group(1)), "./"])
+subprocess.call(["cat", "./TEST_RESULT"])
 
 
 ### virt-log - projdi si manual na zjisteni IP adresy
