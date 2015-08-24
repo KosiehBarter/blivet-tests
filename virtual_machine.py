814da80c1a36c957b3a25b28276649b319303bc7
Karel Valek <kvalek@redhat.com>
2015-08-24 16:44:59 +0200

    Added draft for snapshot revert
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- virtual_machine.py
+++ virtual_machine.py
@@ -20,37 +20,42 @@ import glob
 import time
 import re
 
-#
+
 scp_source = "/home/kvalek/GitHub/blivet-tests/"
 
+# check if machine exists
+vys = subprocess.getoutput("virsh list --all | grep {}".format(mach_name))
 
-# Destroy the machine if needed
-#subprocess.call(["virsh", "destroy", "{}".format(mach_name)])
-#subprocess.call(["virsh", "undefine", "{}".format(mach_name)])
+if vys == "":
+    # undefine domain if needed
+    subprocess.call(["virsh destroy {}".format(mach_name)], shell=True)
+    subprocess.call(["virsh undefine {}".format(mach_name)], shell=True)
 
-#subprocess.call(["virt-install", "--name", "{}".format(mach_name),\
-#"--disk", "{},size=8".format(full_disk_path_1),\
-#"--disk", "{},size=2".format(full_disk_path_2),\
-#"--disk", "{},size=2".format(full_disk_path_3),\
-#"--location", "{}".format(full_loc_path),\
-#"--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path), "--noreboot"])
+    # begin creation and installation
+    subprocess.call(["virt-install", "--name", "{}".format(mach_name),\
+    "--disk", "{},size=8".format(full_disk_path_1),\
+    "--disk", "{},size=2".format(full_disk_path_2),\
+    "--disk", "{},size=2".format(full_disk_path_3),\
+    "--location", "{}".format(full_loc_path),\
+    "--ram", "{}".format(ram_size), "-x", "ks={}".format(ks_file_path), "--noreboot"])
 
-
-#subprocess.call(["virsh", "start", install_name])
-#time.sleep(20)
+subprocess.call(["virsh", "start", install_name])
+time.sleep(60)
 ip_address = subprocess.getoutput("virt-log -d f23 | grep bound | tail -n 1")
 vystup = re.search(r'.*to\ ([0-9]*.[0-9]*.[0-9*]*.[0-9]*).*', ip_address)
 if vystup:
     print(vystup.group(1))
 print(ip_address)
 file_list = glob.glob("{}*".format(scp_source))
+
+# scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+# copy files to machine
 for inc in file_list:
-    subprocess.call(["scp", "-i", "{}".format(key_location), inc, "{}:~".format(vystup.group(1))])
-subprocess.call(["virsh", "snapshot-create-as", install_name, "--disk-only", "--atomic"])
-subprocess.call(["ssh", "-i", "{}".format(key_location), "root@{}:~".format(vystup.group(1)), "'python3'","'test_arrays.py'"])
+    subprocess.call(["scp", "-o", "UserKnownHostsFile=/dev/null", "-o", "StrictHostKeyChecking=no", "-i", "{}".format(key_location), inc, "{}:~".format(vystup.group(1))])
+subprocess.call(["virsh", "snapshot-create", "{}".format(mach_name), "start-snap", "--disk-only", "--atomic"])
+
+# run test
+subprocess.call(["ssh", "-i", "{}".format(key_location), "root@{}".format(vystup.group(1)), "'python3'","'test_arrays.py'"])
 subprocess.call(["scp", "-i", "{}".format(key_location), "root@{}:/root/TEST_RESULT".format(vystup.group(1)), "./"])
+subprocess.call(["virsh snapshot-revert --domain {} {}".format(mach_name, "start-snap")], shell=True)
 subprocess.call(["cat", "./TEST_RESULT"])
-
-
-### virt-log - projdi si manual na zjisteni IP adresy
-# ip_address = subprocess.call(["virt-log", "-d", mach_name, "|", "grep", "bound", "|", "tail", "-n", "1", "sed", "-r", "\'s/.*to\ ([0-9]\{3\}\.[0-9]\{3\}\.[0-9]\{3\}\.[0-9]\{3\}).*/\1/\'"])
