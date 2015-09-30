d3d00eadf98867d4e39a2b49c677fe4b191f7ba8
Karel Valek <kvalek@redhat.com>
2015-09-30 08:11:29 +0200

    Upgraded to separate classes_*
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- checker_test_utils.py
+++ checker_test_utils.py
@@ -3,36 +3,39 @@
 ### Author: kvalek@redhat.com
 ### This program is under GPL licence.
 
+## Basic disk test
 def test_properties_disk(sys_scan, blv_scan):
     lt = ["NAME", "PATH", "REMOVABLE", "VENDOR", "SIZE"]
     ia = []
     ss = [sys_scan.name, sys_scan.system_path, sys_scan.removable, sys_scan.vendor, sys_scan.space]
     bs = [blv_scan.b_name, blv_scan.b_system_path, blv_scan.b_removable, blv_scan.b_vendor, blv_scan.b_space]
-
     for inc in range(len(ss)):
         if (ss[inc] != bs[inc]):
             ia.append("FAIL: {}\t {} != {}".format(lt[inc], ss[inc], bs[inc]))
     return ia
 
-def print_properties(ia):
-    for inc in range(len(ia)):
-        print(ia[inc])
-
-def test_properties_partition(sys_scan, blv_scan):
-    lt = ["NAME", "PATH", "PART_FORMAT", "PART_SIZE", "PART_START", "PART_END", "UUID"]
+## Check formatting with fdisk / parted
+def check_formatting(sys_scan, blv_scan):
     ia = []
-    ss = [sys_scan.part_name, sys_scan.part_system_path, sys_scan.part_format, sys_scan.part_size, sys_scan.part_start, sys_scan.part_end, sys_scan.part_uuid]
-    bs = [blv_scan.b_part_name, blv_scan.b_part_system_path, blv_scan.b_part_format, blv_scan.b_part_size, blv_scan.b_part_start, blv_scan.b_part_end, blv_scan.b_part_uuid]
+    if(sys_scan.alloc_type != blv_scan.b_alloc_type):
+        ia.append("FAIL:\tALLOC_TYPE\t{} != {}".format(sys_scan.alloc_type, blv_scan.b_alloc_type))
+    if(sys_scan.disk_uuid != blv_scan.b_disk_uuid):
+        ia.append("FAIL:\tDISK_UUID\t{} != {}".format(sys_scan.disk_uuid, blv_scan.b_disk_uuid))
+    return ia
+
+## Check partitions
+def test_properties_partition(sys_scan, blv_scan):
+    lt = ["PART_NAME", "PART_UUID", "PART_SIZE", "PART_SEC_START", "PART_SEC_END"]
+    ia = []
+    ss = [sys_scan.sd_part_name, sys_scan.sd_part_uuid, sys_scan.sd_part_size, sys_scan.sd_part_start, sys_scan.sd_part_end]
+    bs = [blv_scan.bd_part_name, blv_scan.bd_part_uuid, blv_scan.bd_part_size, blv_scan.bd_part_start, blv_scan.bd_part_end]
 
     for inc in range(len(ss)):
         if (ss[inc] != bs[inc]):
             ia.append("FAIL: {}\t\t: {} != {}".format(lt[inc], ss[inc], bs[inc]))
     return ia
 
-def check_formatting(sys_scan, blv_scan):
-    ia = []
-    if(sys_scan.alloc_type != blv_scan.b_alloc_type):
-        ia.append("FAIL:\tALLOC_TYPE\t{} != {}".format(sys_scan.alloc_type, blv_scan.b_alloc_type))
-    if(sys_scan.disk_uuid != blv_scan.b_disk_uuid):
-        ia.append("FAIL:\tDISK_UUID\t{} != {}".format(sys_scan.disk_uuid, blv_scan.b_alloc_type))
-    return ia
+## Print function
+def print_properties(ia):
+    for inc in range(len(ia)):
+        print(ia[inc])
