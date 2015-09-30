2975d38ad40d83e03a83739f2d5fb5db7a1d10c0
Karel Valek <kvalek@redhat.com>
2015-09-30 16:49:11 +0200

    Obsolete code removed
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_arrays.py
+++ test_arrays.py
@@ -38,16 +38,6 @@ ia = checker_test_utils.test_properties_partition(test_system_partition, test_bl
 write_issues("\nSingle partition", ia)
 
 """
-print("\nPartitioned disk check - Single partition")
-
-ia = checker_test_utils.test_properties_partition(test_system_partition, test_blivet_partition)
-checker_test_utils.print_properties(ia)
-soubor = open("TEST_RESULT", "a+")
-soubor.write("Partitioned disk check - Single partition")
-if(ia != None):
-    soubor.write(ia)
-soubor.close()
-
 print("\nPartitioned disk check - Single Formatted")
 partition_formatter = classes.SystemPartitionFormatted('vdb', 1, 1, -1, "MiB", "primary", "ext4")
 test_system_partition = classes.SystemPartition('vdb', 1, 1, -1, "MiB", "primary")
