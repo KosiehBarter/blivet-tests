3e34ba3f8d3883117ea9b31623989a7ccc99c560
Karel Valek <kvalek@redhat.com>
2015-10-06 16:39:28 +0200

    Separated test architecture - test 1
    Signed-off-by: Karel Valek <kvalek@redhat.com>

---
+++ test_stage1.py
@@ -0,0 +1,12 @@
+### Test - stage 1
+### Part of: Blivet test collection
+### Author: kvalek@redhat.com
+### This program is under GPL licence.
+
+import classes
+import test_utils
+
+test_system = classes.SystemDisk_Scan('vdb')
+test_blivet = classes.BlivetInitialization('vdb').disk
+ia = test_utils.test(test_system, test_blivet)
+test_utils.write_issues(ia, "Basic disk")
