69665b8f6cc35a4902b6cc86e6e6b8c2e14003c6
Karel Valek <kvalek@redhat.com>
2015-10-06 16:40:00 +0200

    New test arch - stage 2
    Signed-off-by: Karel Valek <kvalek@redhat.com>

---
+++ test_stage2.py
@@ -0,0 +1,15 @@
+### Test - stage 2
+### Part of: Blivet test collection
+### Author: kvalek@redhat.com
+### This program is under GPL licence.
+
+import classes
+import checker_test_utils
+import test_utils
+
+test_utils.create_new_alloc_table("vdb")
+
+test_system_formatted = classes.SystemDiskFormatted_Scan('vdb')
+test_blivet_formatted = classes.BlivetInitialization('vdb').disk
+ia = test_utils.test(test_system_formatted, test_blivet_formatted)
+test_utils.write_issues(ia, "Formatted disk")
