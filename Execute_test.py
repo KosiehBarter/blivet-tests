e2c44c554d0dffb66de7d1b5f3a74ef903d21192
Karel Valek <kvalek@redhat.com>
2015-10-14 15:54:38 +0200

    Added support for reading from config ini
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- Execute_test.py
+++ Execute_test.py
@@ -4,19 +4,40 @@
 ### This program is under GPL licence.
 
 import virtual_machine
+import configparser
 import subprocess
+import test_utils
 
-machine_name = "fedora_23"
-machine_full_path = "/home/Libvirt_disks/"
-machine_ks_full_path = "http://cobra02/ks/kv/autoInstaller.ks"
-machine_iso_full_path = "/home/Libvirt_disks/f23.iso"
-machine_snap_name = "snap-start"
+## Parse INI
+conf_object = configparser.ConfigParser()
+conf_object.read('blivet-tests.ini')
 
-ssh_full_path = "/home/kvalek/.ssh/atlas"
-scp_copy_source = "/home/kvalek/GitHub/blivet-tests/"
+machine_name = conf_object['MACHINE']['MachineName']
+machine_ram = conf_object['MACHINE']['MachineRAM']
+machine_no_of_disks = int(conf_object['MACHINE']['MachineNoOfDisks'])
+machine_snap_name = conf_object['MACHINE']['MachineSnapshotName']
 
+machine_full_path = conf_object['PATHS']['MachineInstallPath']
+machine_ks_full_path = conf_object['PATHS']['MachinePathToKickstart']
+machine_iso_full_path = conf_object['PATHS']['MachinePathToIso']
+
+ssh_key = conf_object['SSH']['SSHKeyName']
+ssh_full_path = "{}{}".format(conf_object['SSH']['SSHFullPath'], ssh_key)
+scp_copy_source = conf_object['SSH']['SCPCopySource']
+
+ks_repo = conf_object['KICKSTART']['Repository']
+ks_additionalrepo = conf_object['KICKSTART']['AdditionalRepository']
+ks_keyboard = conf_object['KICKSTART']['Keyboard']
+ks_timezone = conf_object['KICKSTART']['Timezone']
+ks_rootpass = conf_object['KICKSTART']['RootPassword']
+
+
+## Create & stat virtual machine
 if(subprocess.call(["LANG=c virsh list --all | grep {}".format(machine_name)], shell=True) != 0):
-    virtual_machine.create_machine(machine_name, 2048, machine_full_path, machine_ks_full_path, machine_iso_full_path, 2)
+    subprocess.call(["ssh-keygen -t rsa -N \"\" -f {}SSH/{}".format(machine_full_path, machine_name)], shell=True)
+    ks_sshkey = subprocess.getoutput("cat {}/SSH/{}.pub".format(machine_full_path, machine_name))
+    ks_full_path = test_utils.make_kickstart(machine_full_path, machine_name, ks_repo, ks_keyboard, ks_timezone, ks_rootpass, ks_sshkey, ks_additionalrepo)
+    virtual_machine.create_machine(machine_name, machine_ram, machine_full_path, machine_ks_full_path, machine_iso_full_path, machine_snap_name, machine_no_of_disks)
 
 if(subprocess.call(["LANG=c virsh list --all | grep {} | grep running".format(machine_name)], shell=True) != 0):
     virtual_machine.start_machine(machine_name)
