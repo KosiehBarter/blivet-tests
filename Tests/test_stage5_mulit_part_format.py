d8190b9d10e826e83d3d7baf007702a85786780e
Karel Valek <kvalek@redhat.com>
2015-10-15 12:15:36 +0200

    Renamed for better recognition, moved to separate
    Signed-off-by: Karel Valek <kvalek@redhat.com>

---
+++ Tests/test_stage5_mulit_part_format.py
@@ -0,0 +1,43 @@
+### Test - stage 5
+### Part of: Blivet test collection
+### Author: kvalek@redhat.com
+### This program is under GPL licence.
+import sys
+sys.path.append("..")
+sys.path.append("./Test_Deps")
+
+import classes
+import test_utils
+
+
+test_utils.create_new_alloc_table("vdb")
+
+
+## Multi partition test
+start = 1
+finish = 512 + start
+
+
+## Test partitions
+list_of_tests = []
+list_of_blivet = []
+list_of_ia = []
+
+for inc in range(4):
+    if inc == 3:
+        test_utils.create_new_partition("vdb", "primary", start, -1)
+    else:
+        test_utils.create_new_partition("vdb", "primary", start, finish)
+        start = finish + 1
+        finish = start + 512
+    test_utils.format_new_partition("{}{}".format("vdb", inc + 1), "ext4")
+
+    ## init objects
+    list_of_tests.append(classes.SystemPartitionFormatted_Scan('vdb', inc + 1))
+    list_of_blivet.append(classes.BlivetInitialization('vdb', inc + 1).child)
+
+    ## Store in arrays
+    list_of_ia.append(test_utils.test(list_of_tests[inc], list_of_blivet[inc]))
+
+    ## Store in file.
+    test_utils.write_issues(list_of_ia[inc], "Multi partition - part {}".format(inc + 1), 5)
---
+++ Tests/test_stage7_logical_multi.py
@@ -0,0 +1,40 @@
+### Test - stage 7
+### Part of: Blivet test collection
+### Author: kvalek@redhat.com
+### This program is under GPL licence.
+import sys
+sys.path.append("..")
+sys.path.append("./Test_Deps")
+
+import classes
+import test_utils
+
+test_utils.create_new_alloc_table("vdb")
+
+## Multi partition test
+start = 1
+finish = 512 + start
+
+## Test partitions
+list_of_tests = []
+list_of_blivet = []
+list_of_ia = []
+
+test_utils.create_new_partition("vdb", "extended", start, -1)
+for inc in range(4):
+    if inc == 3:
+        test_utils.create_new_partition("vdb", "logical", start + 1, -1)
+    else:
+        test_utils.create_new_partition("vdb", "logical", start + 1, finish)
+        start = finish + 1
+        finish = start + 512
+
+    ## init objects
+    list_of_tests.append(classes.SystemLogical_Scan('vdb', inc + 1))
+    list_of_blivet.append(classes.BlivetInitialization('vdb', inc + 1).child)
+
+    ## Store in arrays
+    list_of_ia.append(test_utils.test(list_of_tests[inc], list_of_blivet[inc]))
+
+    ## Store in file.
+    test_utils.write_issues(list_of_ia[inc], "Multi partition - part {}".format(inc + 1), 5)
