fe48553edd3c7431917c8945cd24990ba5880e7d
Karel Valek <kvalek@redhat.com>
2015-10-15 12:12:00 +0200

    Draft: Removed kickstart generator, plan: separate
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- Execute_test.py
+++ Execute_test.py
@@ -2,10 +2,13 @@
 ### Part of: Blivet test collection
 ### Author: kvalek@redhat.com
 ### This program is under GPL licence.
+import sys
+sys.path.append("./Test_Deps/")
 
+
+import subprocess
 import virtual_machine
 import configparser
-import subprocess
 import test_utils
 
 ## Parse INI
@@ -32,11 +35,11 @@ ks_timezone = conf_object['KICKSTART']['Timezone']
 ks_rootpass = conf_object['KICKSTART']['RootPassword']
 
 
-## Create & stat virtual machine
+## Create & start virtual machine
 if(subprocess.call(["LANG=c virsh list --all | grep {}".format(machine_name)], shell=True) != 0):
-    subprocess.call(["ssh-keygen -t rsa -N \"\" -f {}SSH/{}".format(machine_full_path, machine_name)], shell=True)
-    ks_sshkey = subprocess.getoutput("cat {}/SSH/{}.pub".format(machine_full_path, machine_name))
-    ks_full_path = test_utils.make_kickstart(machine_full_path, machine_name, ks_repo, ks_keyboard, ks_timezone, ks_rootpass, ks_sshkey, ks_additionalrepo)
+    #subprocess.call(["ssh-keygen -t rsa -N \"\" -f {}SSH/{}".format(machine_full_path, machine_name)], shell=True)
+    #ks_sshkey = subprocess.getoutput("cat {}/SSH/{}.pub".format(machine_full_path, machine_name))
+    #ks_full_path = test_utils.make_kickstart(machine_full_path, machine_name, ks_repo, ks_keyboard, ks_timezone, ks_rootpass, ks_sshkey, ks_additionalrepo)
     virtual_machine.create_machine(machine_name, machine_ram, machine_full_path, machine_ks_full_path, machine_iso_full_path, machine_snap_name, machine_no_of_disks)
 
 if(subprocess.call(["LANG=c virsh list --all | grep {} | grep running".format(machine_name)], shell=True) != 0):
