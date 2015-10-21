42993d22a62cfdc7ce3a8f33d51346ec1c6b0612
Karel Valek <kvalek@redhat.com>
2015-10-21 15:57:47 +0200

    Fixed major issue related to ini parsing
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- virtual_machine.py
+++ virtual_machine.py
@@ -8,95 +8,81 @@ import glob
 import time
 import re
 
-## Create a machine function.
-#def create_machine(install_name, ram_size, disk_path, kickstart, iso_file, machine_snap_name, additional_disks = 1):
-def create_machine(install_name, ram_size, disk_path, kickstart, iso_file, machine_snap_name, additional_disks = 1):
-    if(install_name == "" or ram_size == 0, disk_path == "" or iso_file == ""):
-        print("Please specify all parameters:\n\
-\tinstall_name - name of the machine to perform a test on\n\
-\tadditional_disks - Number of additional disks. 1 added automatically, size is set to 2 G for others.\n\
-\tram_size - amount of RAM for the machine\n\
-\tdisk_path - full path for disks and isos\n\
-\t\tNOTE: Specify full path, eg. /home/Libvirt_disks/, including ending slash.\n\
-\tkickstart - kickstart path or URL.\n\
-\tiso_file - used with disk_path, specifies iso for install. Specify full name.")
-    print(kickstart)
 
-    # undefine domain if needed
-    subprocess.call(["virsh destroy {}".format(install_name)], shell=True)
-    subprocess.call(["virsh undefine {}".format(install_name)], shell=True)
-
-    ## Determine number of disks
+## Create array of disk arguments
+def create_disks(additional_disks, machine_full_path, machine_name):
     disk_list = []
     letter_counter = 98
     for inc in range(additional_disks):
-        disk_list.append("--disk \"{}{}_vd{},size=2\"".format(disk_path, install_name, chr(letter_counter)))
+        disk_list.append("--disk \"{}{}_vd{},size=2\"".format(machine_full_path, machine_name, chr(letter_counter)))
         letter_counter = letter_counter + 1
     disk_arg = " ".join(disk_list)
-
-    # begin creation and installation
-    subprocess.call(["virt-install --name {}\
-    --disk \"{}{}_vda,size=8\" \
-    {}\
-    --location {} \
-    --graphics vnc,listen=0.0.0.0 --noautoconsole \
-    --ram {} -x ks={} --noreboot\
-    ".format(install_name, disk_path, install_name, disk_arg, iso_file, ram_size, kickstart)], shell=True)
-
-    ### Wait until the machine is powered off. Try to ssh if installed.
-    while (subprocess.call(["virsh list | grep {} > /dev/null".format(install_name)], shell=True) != 1):
-        time.sleep(10)
-
-    start_machine(install_name)
-
-    ## Create a snapshot of the machine.
-    subprocess.call(["virsh snapshot-create-as {} {}".format(install_name, machine_snap_name)], shell=True)
-    # subprocess.call(["virsh shutdown {}".format(install_name)], shell=True)
+    return disk_arg
 
 
-## Start the machine.
-def start_machine(install_name):
-    subprocess.call(["virsh", "start", install_name])
-    while (subprocess.call(["virt-log -d {} | grep bound > /dev/null".format(install_name)], shell=True) != 0):
-        time.sleep(5)
+## Create and install machine
+def create_machine(machine_name, machine_full_path, disk_arg, machine_iso_full_path, machine_ram, machine_ks_full_path, machine_snap_name):
+    subprocess.call(["virt-install --name {} --disk \"{}{}_vda,size=8\" {} --location {} --graphics vnc,listen=0.0.0.0 --noautoconsole --ram {} -x ks={}".format(machine_name, machine_full_path, machine_name, disk_arg, machine_iso_full_path, machine_ram, machine_ks_full_path)], shell = True)
 
-## Find IP address of the machine and return it.
-def find_ip_address(install_name):
+    out = 0
+    while out != 0:
+        time.sleep(1)
+        out = subprocess.call(["virsh list | grep {} > /dev/null".format(machine_name)], shell=True)
+
+    while (subprocess.call(["virsh list | grep {} > /dev/null".format(machine_name)], shell=True) != 1):
+        time.sleep(1)
+
+    subprocess.call(["virsh snapshot-create-as {} {}".format(machine_name, machine_snap_name)], shell=True)
+
+
+## Start, and wait for the machine
+def start_machine(machine_name):
+    subprocess.call(["virsh start {}".format(machine_name)], shell = True)
+
+    while subprocess.call(["virt-log -d {} | grep bound".format(machine_name)], shell = True) != 0:
+        time.sleep(1)
+
+
+## Get machine's IP address.
+def find_ip_address(machine_name):
     ## Regex the IP address from log and store it to variable
-    ip_address = subprocess.getoutput("LANG=c virt-log -d {} | grep bound | tail -n 1".format(install_name))
+    ip_address = subprocess.getoutput("LANG=c virt-log -d {} | grep bound | tail -n 1".format(machine_name))
+    print(ip_address)
     vystup = re.search(r'.*to\ ([0-9]*.[0-9]*.[0-9*]*.[0-9]*).*', ip_address)
     if vystup:
         return vystup.group(1)
 
 
+## Revert back machine
+def revert_machine(machine_name, machine_snap_name):
+    out = subprocess.call(["virsh snapshot-revert {} {}".format(machine_name, machine_snap_name)], shell=True)
+    if out != 0:
+        print("ERROR:\tSnapshot \"{}\" for machine \"{}\" failed to revert.".format(machine_snap_name, machine_name))
+    else:
+        print("Machine \"{}\" successfully reverted to snapshot \"{}\".".format(machine_name, machine_snap_name))
+
+
+## Return tuple of lists, test stages and deps
+def get_scp_files(scp_copy_source):
+    test_list = glob.glob("{}Tests/*.py".format(scp_copy_source))
+    deps_list = glob.glob("{}Test_Deps/*.py".format(scp_copy_source))
+    return (test_list, deps_list)
+
+
 ## Copy files to machine
-def copy_files(ip_address, copy_path, ssh_cred, return_bool = False):
-    file_list = glob.glob("{}/Tests/*".format(copy_path))
-    subprocess.call(["scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i {} -r {}/* root@{}:~/".format(ssh_cred, copy_path, ip_address)], shell=True)
-    #for inc in file_list:
-    #    subprocess.call(["scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i {} {} root@{}:/root/".format(ssh_cred, inc, ip_address)], shell=True)
-    if return_bool == True:
-        return file_list
+def scp_copy_files(ip_address, ssh_full_path, scp_copy_source, test_list, deps_list, copy_result = False):
+    if copy_result == True:
+        if (subprocess.call(["ls {}Test_Results".format(scp_copy_source)], shell=True) != 0):
+            subprocess.call(["mkdir {}Test_Results".format(scp_copy_source)], shell=True)
+
+        subprocess.call(["scp -o StrictHostKeyChecking=no -i {} root@{}:/root/TEST_RESULT* {}Test_Results/".format(ssh_full_path, ip_address, scp_copy_source)], shell=True)
+    ## If copy_result == False
+    else:
+        subprocess.call(["ssh -o StrictHostKeyChecking=no -i {} root@{} 'mkdir ~/Tests && mkdir ~/Test_Deps'".format(ssh_full_path, ip_address)], shell=True)
+        subprocess.call(["scp -o StrictHostKeyChecking=no -i {} {} root@{}:~/Tests/".format(ssh_full_path, " ".join(test_list), ip_address)], shell=True)
+        subprocess.call(["scp -o StrictHostKeyChecking=no -i {} {} root@{}:~/Test_Deps/".format(ssh_full_path, " ".join(deps_list), ip_address)], shell=True)
 
 
-## Run specified test on a machine with specified IP
-def run_test(ip_address, stage_list, scp_copy_source, ssh_full_path, install_name, machine_snap_name):
-    if subprocess.call(["ls {}Test_Output".format(scp_copy_source)], shell=True) != 0:
-        subprocess.call(["mkdir {}Test_Output".format(scp_copy_source)], shell=True)
-
-    counter = 1
-    for inc in stage_list:
-        start_machine(install_name)
-        copy_files(ip_address, scp_copy_source, ssh_full_path)
-
-        if subprocess.call(["ssh -o StrictHostKeyChecking=no -i {} root@{} 'ls ~/Tests/test_stage{}*.py > /dev/null'".format(ssh_full_path, ip_address, counter)], shell=True) == 0:
-            subprocess.call(["ssh -o StrictHostKeyChecking=no -i {} root@{} 'python3 ~/Tests/test_stage{}*.py'".format(ssh_full_path, ip_address, counter)], shell=True)
-            subprocess.call(["scp -o StrictHostKeyChecking=no -i {} root@{}:~/TEST_RESULT_{} {}Test_Output/".format(ssh_full_path, ip_address, counter, scp_copy_source)], shell=True)
-            subprocess.call(["virsh shutdown {}".format(install_name)], shell=True)
-            counter = counter + 1
-            out = subprocess.call(["virsh snapshot-revert {} {}".format(install_name, machine_snap_name)], shell=True)
-            if out != 0:
-                return "FAIL:\tVIRSH FAILED TO REVERT TO \"{}\"".format(machine_snap_name)
-        else:
-            break
-    return
+## Initiate test stage
+def initiate_test(ip_address, ssh_full_path, test_num):
+    subprocess.call(["ssh -o StrictHostKeyChecking=no -i {} root@{} 'python3 /root/Tests/test_stage{}*'".format(ssh_full_path, ip_address, test_num)], shell=True)
