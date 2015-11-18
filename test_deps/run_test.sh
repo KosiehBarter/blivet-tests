f4dc2462178adf2fa501dc3d5f4c38bbe0fa8499
Karel Valek <kvalek@redhat.com>
2015-11-18 15:32:52 +0100

    Fixed calling stages and auto poweroff
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_deps/run_test.sh
+++ test_deps/run_test.sh
@@ -2,4 +2,8 @@
 
 cd /root/
 test_to_run=`ls test_stage*.py`
-python3 $test_to_run && poweroff
+test_stage=`echo $test_to_run | sed 's/test_stage\([0-9]*\)_.*/\1/'`
+
+python3 $test_to_run
+
+poweroff
