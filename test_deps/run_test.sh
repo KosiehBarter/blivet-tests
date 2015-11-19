a75b0fb36860f43df0a9c63d53de5f5c51c2710c
Karel Valek <kvalek@redhat.com>
2015-11-19 11:30:23 +0100

    Upgraded to meet new test_stage_X_* recognition
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_deps/run_test.sh
+++ test_deps/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /root/
-test_to_run=`ls test_stage*.py`
-test_stage=`echo $test_to_run | sed 's/test_stage\([0-9]*\)_.*/\1/'`
+test_to_run=`ls test_stage_*.py`
+test_stage=`echo $test_to_run | sed 's/test_stage_\([0-9]*\)_.*/\1/'`
 
 python3 $test_to_run
 
