0441edcae57dec8bdbd7d333602e3110cec6d558
Karel Valek <kvalek@redhat.com>
2015-11-23 16:34:46 +0100

    Fixed several functions
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_deps/blivet_utils.py
+++ test_deps/blivet_utils.py
@@ -4,9 +4,25 @@
 ### This program is under GPL licence.
 import blivet
 
+## Init blivet
+def init_blivet_basic():
+    bo = blivet.Blivet()
+    bo.reset()
+    return bo
+
+def init_disk(bo, disk):
+    return bl_obj.devicetree.getDeviceByName(disk)
+
+
 ## Create a msdos label
-def create_disk_label(blivet_object, disk_object, label_type):
-    nev_format = blivet.formats.getFormat("disklabel", device=disk_object.path,
+def create_disk_label(bo, disk_object, label_type):
+    new_format = blivet.formats.getFormat("disklabel", device=disk_object.path,
                                             labelType = label_type)
-    blivet.formatDevice(disk_object, new_format)
-    blivet_object.doIt()
+
+    bo.formatDevice(disk_object, new_format)
+    bo.doIt()
+
+def create_partition(bo, part_size, disk_obj):
+    new_part = bo.newPartition(size=int(part_size), parents=[disk_obj])
+    bo.createDevice(new_part)
+    blivet.partitioning.doPartitioning(bo)
