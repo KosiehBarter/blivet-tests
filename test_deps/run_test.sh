3660c70e6b27b2b8dbfeddef78774e596ea65afc
Karel Valek <kvalek@redhat.com>
2015-11-24 18:12:05 +0100

    Added condition (working?) for failed test
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_deps/run_test.sh
+++ test_deps/run_test.sh
@@ -4,6 +4,10 @@ cd /root/
 test_to_run=`ls test_stage_*.py`
 test_stage=`echo $test_to_run | sed 's/test_stage_\([0-9]*\)_.*/\1/'`
 
+if [ $? -ne 0 ] ; then
+  echo "$test_to_run FAILED to run" > TEST_RESULT_$test_stage ;
+fi
+
 python3 $test_to_run
 
 poweroff
