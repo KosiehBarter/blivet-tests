83e9b515eff134f78868929141cfce65bbcffbf4
Karel Valek <kvalek@redhat.com>
2015-11-24 18:13:38 +0100

    Unknown fix / feature added
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_deps/blivet_utils.py
+++ test_deps/blivet_utils.py
@@ -22,7 +22,11 @@ def create_disk_label(bo, disk_object, label_type):
     bo.formatDevice(disk_object, new_format)
     bo.doIt()
 
-def create_partition(bo, part_size, disk_obj):
-    new_part = bo.newPartition(size=int(part_size), parents=[disk_obj])
+def create_partition(log_stage, bo, disk_obj):
+    log_stage.debug("Calling: blivet.partitioning.get_free_regions on {}, align=True".format(disk_obj.name))
+    part_size = blivet.partitioning.getFreeRegions([disk_obj], align=True)
+    log_stage.debug("part_size = {}".format(part_size))
+
+    new_part = bo.newPartition(size=part_size[0].getSize(), parents=[disk_obj])
     bo.createDevice(new_part)
     blivet.partitioning.doPartitioning(bo)
