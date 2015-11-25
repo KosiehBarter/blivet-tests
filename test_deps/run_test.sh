70c2bef18a5887f710b288094c4af29ab4227848
Karel Valek <kvalek@redhat.com>
2015-11-25 13:39:18 +0100

    Added support for future -update switch
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- test_deps/run_test.sh
+++ test_deps/run_test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 cd /root/
-test_to_run=`ls test_stage_*.py`
-test_stage=`echo $test_to_run | sed 's/test_stage_\([0-9]*\)_.*/\1/'`
 
-if [ $? -ne 0 ] ; then
+## Check, if run_update is present, if so, run it
+ls ./run_update.sh
+
+if [ $? == 0 ] ; then
+  /bin/bash /root/run_update.sh ;
+else
+  test_to_run=`ls test_stage_*.py`
+  test_stage=`echo $test_to_run | sed 's/test_stage_\([0-9]*\)_.*/\1/'` ;
+fi
+
+if [ $? != 0 ] ; then
   echo "$test_to_run FAILED to run" > TEST_RESULT_$test_stage ;
 fi
 
